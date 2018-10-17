b6e76c415f217cf122fab67580d728185cc7b5bd
Totul <rytotul@gmail.com>
2018-10-17 22:54:36 +0600

    Added Pop

--- Youtube2mp3.user.js
+++ Youtube2mp3.user.js
@@ -15,30 +15,31 @@
 // @include         http://www.easy-youtube-mp3.com/*
 // @include         https://www.easy-youtube-mp3.com/*
 //
-// @version         0.3
+// @version         0.4 (Beta)
 // @updateURL       https://github.com/rytotul/Youtube2mp3/raw/master/Youtube2mp3.user.js
 //
 // @run-at          document-end
 // @unwrap
 // ==/UserScript==
 
-var easy_btn_onclick = function (){
-  var path ='https://www.easy-youtube-mp3.com/download.php'+window.location.search;
-  //window.open(path,'_blank');
 
+// Button That Captures the click and do the work
+var easy_btn_onclick = function (){
+  var path ='https://www.easy-youtube-mp3.com/download.php'+window.location.search; // path to download link
+
+  // Some window measurement capturing
+  var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
+  var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
   var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
   var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+  var left = ((width / 2) - (700 / 2)) + dualScreenLeft;
+  var top = ((height / 2) - (700 / 2)) + dualScreenTop;
 
-  var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-  var top = ((height / 2) - (h / 2)) + dualScreenTop;
-
-  newwindow=window.open(path,"Qr Code",'height=400,width=400,top='+top+',left='+left);
+  // Property for new po-up window
+  var newwindow = window.open(path,"Qr Code",'height=400,width=400,top='+top+',left='+left);
   if (window.focus) {newwindow.focus()}
   return false;
 };
-
-
-
 
 var getSpan = function(text, className) {
     var _tn = document.createTextNode(text);
@@ -48,6 +49,7 @@ var getSpan = function(text, className) {
     return span;
 };
 
+// injecting interface to youtube
 var myAppInterface = {
   init:function(){
     this.insertGlobalCSS()

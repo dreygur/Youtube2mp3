c7d2952cd4d1a887a95848f425cff47513867ab9
Rakibul Yeasin <rytotul@gmail.com>
2018-12-14 17:14:51 +0600

    Changed download server and bug fix

--- Youtube2mp3.user.js
+++ Youtube2mp3.user.js
@@ -15,7 +15,7 @@
 // @include         http://www.easy-youtube-mp3.com/*
 // @include         https://www.easy-youtube-mp3.com/*
 //
-// @version         1.0.3
+// @version         1.0.4
 // @updateURL       https://github.com/rytotul/Youtube2mp3/raw/master/Youtube2mp3.user.js
 //
 // @run-at          document-end
@@ -25,65 +25,89 @@
 
 // Button That Captures the click and do the work
 function ytBtnOnclick() {
-  // Some window measurement capturing
-  var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
-  var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
-  var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-  var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
-  var left = ((width / 2) - (700 / 2)) + dualScreenLeft;
-  var top = ((height / 2) - (700 / 2)) + dualScreenTop;
-
-  // Property for new po-up window
-  var url = "www.convertmp3.io/fetch/?video="+window.location.href;
-  var qr_code = "https://chart.googleapis.com/chart?cht=qr&chl="+url+"&chs=350x350&chld=L|0";
-  var newwindow = window.open("","Qr Code",'height=400,width=400,top='+top+',left='+left);
-  newwindow.document.write('<title>YouTube 2 mp3</title><iframe style="margin-top:12.5px; margin-left:12.5px; width:370px; height:370px; align:center; border:none;" src="'+qr_code+'"></iframe>');
-  if (window.focus) {newwindow.focus()}
-  return false;
-  };
-
-// injecting interface to youtube
-function getSpan(text, className) {
+    // Some window measurement capturing
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+    var left = ((width / 2) - (700 / 2)) + dualScreenLeft;
+    var top = ((height / 2) - (700 / 2)) + dualScreenTop;
+  
+    // Property for new po-up window
+    var data;
+    var video_id = window.location.search.split('v=')[1];
+    var url = 'https://www.download-mp3-youtube.com/api/?api_key=MjI0NzExODEw&format=mp3&video_id=' + video_id;
+    fetch(url).then(response => {
+        console.log(response);
+        return response;
+    }).then(myJson => {
+        //console.log(myJson.link);
+        var qr_code = "https://chart.googleapis.com/chart?cht=qr&chl="+myJson.link+"&chs=350x350&chld=L|0";
+        //console.log(qr_code);
+        var newwindow = window.open("","Qr Code","height=400,width=400");
+        newwindow.document.write('<title>YouTube 2 mp3</title><iframe style="margin-top:12.5px; margin-left:12.5px; width:370px; height:370px; align:center; border:none;" src="'+qr_code+'"></iframe>');
+        if (window.focus) {newwindow.focus()}
+    });
+    return false;
+  }
+  
+  // injecting interface to youtube
+  function getSpan(text, className) {
     var _tn = document.createTextNode(text);
     var span = document.createElement("span");
     span.className = className;
     span.appendChild(_tn);
     return span;
-};
-
-var myAppInterface = {
-  init:function(){
-    this.insertGlobalCSS()
-  },
-  addGlobalStyle: function(doc, css) {
-    if(document.querySelector('.y2mp3-css'))return;
-    var head = doc.getElementsByTagName('head')[0];
-    if (!head) {return; }
-    var style = doc.createElement('style');
-    style.id = 'y2mp3-css';
-    style.type = 'text/css';
-    if (style.styleSheet) {
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
-    head.appendChild(style);
-  },
-  insertGlobalCSS: function(){
-    var css = function (){
-      /*start
-      #btn-padding{padding-top:10px;}
-      #y2mp3.ytd-watch{padding-top:10px;overflow: auto;padding-bottom: 10px;}
-      #y2mp3 .easy_btn{background-color: #FF0000;border: #FF0000;border-radius: 2px;color: #FFF;padding: 10px 16px; font-size: 1.4em;cursor:pointer;display:inline-block}
-      #y2mp3 .easy_btn:hover{background-color: #a22a2a}
-      @media (min-width: 657px){ytd-watch[theater] #y2mp3.ytd-watch{margin-right:24px}}
-      end*/
-    }.toString().replace("/*start",'').replace("end*/",'').slice(14,-1);
-    this.addGlobalStyle(document, css);
-  },
-}
-
-function createButton() {
+  };
+  
+  var myAppInterface = {
+    init:function(){
+        this.insertGlobalCSS()
+    },
+    addGlobalStyle: function(doc, css) {
+        if (document.querySelector('.y2mp3-css'))return;
+        var head = doc.getElementsByTagName('head')[0];
+        if (!head) {return;}
+        var style = doc.createElement('style');
+        style.id = 'y2mp3-css';
+        style.type = 'text/css';
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+        head.appendChild(style);
+    },
+    insertGlobalCSS: function(){
+        var css = function (){
+            /*start
+            #btn-padding{padding-top:10px;}
+            #y2mp3.ytd-watch {
+              padding-top:10px;
+              overflow: auto;
+              padding-bottom: 10px;
+            }
+            #y2mp3 .easy_btn {
+              background-color: #FF0000;
+              border: #FF0000;
+              border-radius: 2px;
+              color: #FFF;
+              padding: 10px 16px;
+              font-size: 1.4em;
+              cursor:pointer;
+              display:inline-block;
+            }
+            #y2mp3 .easy_btn:hover  {
+              background-color: #a22a2a
+            }
+            @media (min-width: 657px) {ytd-watch[theater] #y2mp3.ytd-watch{margin-right:24px}}
+            end*/
+        }.toString().replace("/*start",'').replace("end*/",'').slice(14,-1);
+        this.addGlobalStyle(document, css);
+    },
+  }
+  
+  function createButton() {
     var btnRow, y2mp3, yBtn;
     var obj = document.getElementById("sponsor-button");
     if(obj === null){
@@ -119,11 +143,12 @@ function createButton() {
             y2mp3.appendChild(yBtn);
         }
     }
-};
-
-// yt does make use of some bogus AJAX functionality which breaks pagemod
-// we have to check in intervals if the document has been replaced by yt to
-// recreate the button if needed.
-var intervalCheck = setInterval(function(){
-   createButton();
-}, 250);
+  };
+  
+  // yt does make use of some bogus AJAX functionality which breaks pagemod
+  // we have to check in intervals if the document has been replaced by yt to
+  // recreate the button if needed.
+  var intervalCheck = setInterval(function(){
+    createButton();
+  }, 4000);
+  
